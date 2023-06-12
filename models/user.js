3ab09f22bcd327e672d4236369cf38156a5371a1
ericbastarache <eric_bastarache@hotmail.com>
2023-06-11 23:02:31 -0400

    Chore: Migrations
    - Add new migration and model for team and organization
    - Change user model to allow individual users to also sign up, not just organizations with teams

--- models/user.js
+++ models/user.js
@@ -10,17 +10,39 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      this.belongsTo(models.Organization, {
+        foreignKey: {
+          name: 'organizationId',
+          allowNull: true
+        },
+        as: 'organization'
+      });
+      this.belongsTo(models.Team, {
+        foreignKey: {
+          name: 'teamId',
+          allowNull: true
+        },
+        as: 'team'
+      });
       this.hasOne(models.Subscription, {
         foreignKey: 'userId',
         as: 'subscription'
-      })
+      });
     }
   }
   User.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     image: DataTypes.STRING,
-    emailVerified: DataTypes.DATE
+    emailVerified: DataTypes.DATE,
+    organizationId: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    teamId: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
   }, {
     sequelize,
     modelName: 'User',
